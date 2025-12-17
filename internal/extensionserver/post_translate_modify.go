c601c93d544231795e613c7902166250ac93b986
Matias Charriere <matias@giantswarm.io>
2025-12-17 11:33:48 +0100

    insert secrets during translation
    Signed-off-by: Matias Charriere <matias@giantswarm.io>

--- internal/extensionserver/post_translate_modify.go
+++ internal/extensionserver/post_translate_modify.go
@@ -2,8 +2,15 @@ package extensionserver
 
 import (
 	"context"
+	"fmt"
 
 	pb "github.com/envoyproxy/gateway/proto/extension"
+	corev3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	tlsv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/transport_sockets/tls/v3"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	"github.com/giantswarm/envoy-extension-server-app/api/v1alpha1"
 )
 
 func (s *Server) PostTranslateModify(ctx context.Context, req *pb.PostTranslateModifyRequest) (*pb.PostTranslateModifyResponse, error) {
@@ -13,15 +20,79 @@ func (s *Server) PostTranslateModify(ctx context.Context, req *pb.PostTranslateM
 	policies := s.extractCertificatePolicies(req.PostTranslateContext.GetExtensionResources())
 
 	s.log.Info("fetched CertificatePolicies", "count", len(policies))
+
+	// Start with the existing secrets from the request
+	secrets := req.Secrets
+
+	// Fetch and add secrets referenced by each policy
 	for _, policy := range policies {
-		s.log.Info("found CertificatePolicy",
+		s.log.Info("processing CertificatePolicy",
 			"name", policy.Name,
 			"namespace", policy.Namespace,
 			"secretName", policy.Spec.SecretName,
 		)
+
+		envoySecret, err := s.fetchAndConvertSecret(ctx, policy)
+		if err != nil {
+			s.log.Error("failed to fetch secret for policy",
+				"policy", policy.Name,
+				"secretName", policy.Spec.SecretName,
+				"error", err,
+			)
+			continue
+		}
+
+		secrets = append(secrets, envoySecret)
+		s.log.Info("added secret to response",
+			"secretName", envoySecret.Name,
+		)
 	}
 
 	return &pb.PostTranslateModifyResponse{
-		Secrets: req.Secrets,
+		Secrets: secrets,
+	}, nil
+}
+
+// fetchAndConvertSecret fetches a K8s TLS secret and converts it to an Envoy Secret.
+func (s *Server) fetchAndConvertSecret(ctx context.Context, policy v1alpha1.CertificatePolicy) (*tlsv3.Secret, error) {
+	var k8sSecret corev1.Secret
+	secretKey := types.NamespacedName{
+		Namespace: policy.Namespace,
+		Name:      policy.Spec.SecretName,
+	}
+
+	if err := s.client.Get(ctx, secretKey, &k8sSecret); err != nil {
+		return nil, fmt.Errorf("failed to get secret %s/%s: %w", secretKey.Namespace, secretKey.Name, err)
+	}
+
+	certChain, ok := k8sSecret.Data[corev1.TLSCertKey]
+	if !ok {
+		return nil, fmt.Errorf("secret %s/%s missing %s key", secretKey.Namespace, secretKey.Name, corev1.TLSCertKey)
+	}
+
+	privateKey, ok := k8sSecret.Data[corev1.TLSPrivateKeyKey]
+	if !ok {
+		return nil, fmt.Errorf("secret %s/%s missing %s key", secretKey.Namespace, secretKey.Name, corev1.TLSPrivateKeyKey)
+	}
+
+	// Use namespace/name format for the Envoy secret name to ensure uniqueness
+	envoySecretName := fmt.Sprintf("%s/%s", secretKey.Namespace, secretKey.Name)
+
+	return &tlsv3.Secret{
+		Name: envoySecretName,
+		Type: &tlsv3.Secret_TlsCertificate{
+			TlsCertificate: &tlsv3.TlsCertificate{
+				CertificateChain: &corev3.DataSource{
+					Specifier: &corev3.DataSource_InlineBytes{
+						InlineBytes: certChain,
+					},
+				},
+				PrivateKey: &corev3.DataSource{
+					Specifier: &corev3.DataSource_InlineBytes{
+						InlineBytes: privateKey,
+					},
+				},
+			},
+		},
 	}, nil
 }
